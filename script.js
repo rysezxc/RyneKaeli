cfbd22d670a6c7ed17988efd82d3da3518ec6053
rysezxc <61983949+rysezxc@users.noreply.github.com>
2026-07-16 23:54:55 +0800

    123

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzfIGtsHBy3l6QdFQatQOq0gwubGMkKIQFDcxKjKz28lONNOk43KSOgr7Gs2Qpck3Gy/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/library/d/1glt7slKg6x4NmjjFI8qUJZM4whULoqTIZSZbZR39kvi8cDMbIQZ5Kh_g/1';
 
 // ─── RSVP Form ───────────────────────────────────────────────────────────────
 const rsvpForm    = document.getElementById('rsvpForm');
